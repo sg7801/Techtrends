62fcef4ac01656609f248063e8d332503fb99114
Srishti Guleria <61888364+sg7801@users.noreply.github.com>
2021-11-21 21:53:44 +0530

    Update app.py

--- techtrends/app.py
+++ techtrends/app.py
@@ -99,5 +99,9 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(level = logging.DEBUG)
+    # set logger to handle STDOUT and STDERR 
+    stdout_handler =  logging.StreamHandler(sys.stdout)`
+    stderr_handler =  logging.StreamHandler(sys.stderr)
+    handlers = [stderr_handler, stdout_handler]
+    logging.basicConfig(level=logging.DEBUG, handlers=handlers)
     app.run(host='0.0.0.0', port='3111')
